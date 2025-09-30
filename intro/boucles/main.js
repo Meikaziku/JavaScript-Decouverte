07f9fb6ee28c72b1a5bf846a2a47dfcbae426f57
Meikaziku <yanis.marouani13@gmail.com>
2025-09-30 15:47:43 +0200

    e

---
+++ intro/boucles/main.js
@@ -0,0 +1,12 @@
+for (let compte = 0; compte < 11; compte++){
+
+    console.log(compte);
+    
+}
+
+let compte2 = [0,1,2,3,4,5,6,7,8,9,10];
+compte2.forEach(number => {
+
+    console.log(number);
+    
+})
--- intro/fizzbuzz/main.js
+++ intro/fizzbuzz/main.js
@@ -17,23 +17,23 @@ if (number % 5 != 0 && number % 3 != 0) {
 }
 
 
-let number = 2;
+// let number = 2;
 
-let resultat = '';
+// let resultat = '';
 
-// si le nombre est un multiple de 3
-if (number % 3 === 0){
-    resultat += 'fizz';
-}
+// // si le nombre est un multiple de 3
+// if (number % 3 === 0){
+//     resultat += 'fizz';
+// }
 
-// si le nombre est un multiple de 5
-if (number % 5 === 0){
-    resultat += 'buzz';
-}
+// // si le nombre est un multiple de 5
+// if (number % 5 === 0){
+//     resultat += 'buzz';
+// }
 
-// si le nombre n'est pas un multiple de 5 ou 3
-if (resultat === ''){
-    resultat = number;
-}
+// // si le nombre n'est pas un multiple de 5 ou 3
+// if (resultat === ''){
+//     resultat = number;
+// }
 
-console.log(resultat);
+// console.log(resultat);
