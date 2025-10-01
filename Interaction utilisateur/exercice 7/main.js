96f17332e2f26b510409d9bd8823c67be06c99bc
Meikaziku <yanis.marouani13@gmail.com>
2025-10-01 16:28:05 +0200

    exercice 7 interaction

---
+++ Interaction utilisateur/exercice 7/main.js
@@ -0,0 +1,7 @@
+let pointure = prompt("notez votre pointure")
+let birthday = prompt("notez votre année de naissance")
+
+let resultat =  (((pointure * 2 + 5)*50)-birthday)+1766
+
+alert(`le resultat est : ${resultat}`)
+
