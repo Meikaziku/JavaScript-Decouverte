213e12d619e8644aea63b5b28fd24a76d93f6d19
Meikaziku <yanis.marouani13@gmail.com>
2025-10-01 15:48:08 +0200

    exercice 4 interaction

---
+++ Interaction utilisateur/exercice 4/main.js
@@ -0,0 +1,5 @@
+let lastName = prompt("Veuillez entrer votre nom");
+let firstName = prompt("Veuillez entrer votre prénom");
+let city = prompt("veuillez entrer votre ville");
+
+alert(`Nom : ${lastName}\nPrénom : ${firstName}\nVille : ${city}`);
