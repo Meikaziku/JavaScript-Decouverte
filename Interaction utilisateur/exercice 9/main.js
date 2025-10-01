b9c7b23b22d17c51aa61bc2d68b4df6051f09be6
Meikaziku <yanis.marouani13@gmail.com>
2025-10-01 16:47:35 +0200

    exercice 9 interaction

---
+++ Interaction utilisateur/exercice 9/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt("veuillez saisir un nombre")
+
+
+for (let index = 1; index <= 10; index++) {
+    let resultat = nombre*index
+    alert(`${nombre}*${index}=${resultat}`)
+    
+}
