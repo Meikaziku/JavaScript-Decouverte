a339f0516c2a12766e1ef90c554e62b9f13e8ac0
Meikaziku <yanis.marouani13@gmail.com>
2025-10-01 16:19:18 +0200

    exercice 5 interaction

---
+++ Interaction utilisateur/exercice 5/main.js
@@ -0,0 +1,6 @@
+let nombre1 = prompt("entrez un premier nombre a virgule")
+let nombre2 = prompt("entre un deuxième nombre a virgule")
+
+let resultat = nombre1 * nombre2
+
+alert(`resultat : ${resultat}`)
