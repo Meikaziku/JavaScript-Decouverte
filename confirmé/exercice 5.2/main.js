a45c5e871242eb7889a06c6bf60ad8bda7ee6e00
Meikaziku <yanis.marouani13@gmail.com>
2025-10-02 15:13:08 +0200

    exercice confirme 5.2

--- confirmé/exercice 5.2/main.js
+++ confirmé/exercice 5.2/main.js
@@ -1,1 +1,24 @@
 
+let isWin = false;
+let nombreDepart;
+
+while (isWin === false) {
+
+  nombreDepart = parseInt(prompt("Devine le nombre !"));
+
+if (nombreDepart > 20) {
+    alert("Plus petit !")
+}
+
+if (nombreDepart < 10) {
+  alert("Plus grand!")
+  
+}
+
+if (nombreDepart > 10 && nombreDepart < 20) {
+  alert("bien joué !")
+  isWin = true
+}
+  
+}
+
