f3fa3364a768d327eac9e27917201e9e2160e372
Meikaziku <yanis.marouani13@gmail.com>
2025-10-02 14:50:52 +0200

    exercice confirme 5.1

---
+++ confirmé/exercice 5.1/main.js
@@ -0,0 +1,15 @@
+let nombre = parseInt(prompt("veuillez saisir un nombre entre 1 et 3"))
+
+if (nombre === 2) {
+
+    alert("La réponse est corrècte")
+    
+} else {
+    alert("La réponse est incorrecte")
+}
+
+
+
+
+
+
