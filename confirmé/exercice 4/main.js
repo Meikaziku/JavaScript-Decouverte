9865e87a742370e00f16109ca8b43947e215f2b9
Meikaziku <yanis.marouani13@gmail.com>
2025-10-02 14:38:02 +0200

    exercice confirme 4

---
+++ confirmé/exercice 4/main.js
@@ -0,0 +1,20 @@
+let amis = ['Zinedinne ','Vincent ','Titouan ','Karim ']
+
+
+
+for (let i = 0; i < amis.length; i++) {
+   
+    
+    let result = amis[i].repeat(i+1)
+    console.log(result);
+
+   
+  
+  
+  
+}
+
+
+
+
+
