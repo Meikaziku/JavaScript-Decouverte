bb55bc54e272d16b84baa90e5cb92b59edbade68
Meikaziku <yanis.marouani13@gmail.com>
2025-10-02 10:07:37 +0200

    exercice 12 interaction

---
+++ Interaction utilisateur/exercice 12/main.js
@@ -0,0 +1,10 @@
+let nombre = prompt("veuillez entrez un nombre");
+
+function calculDuCarre(a) {
+	return a**2;
+	
+	
+}
+
+let resultat = calculDuCarre(nombre)
+alert(`le carré de ${nombre} est ${resultat}`)
