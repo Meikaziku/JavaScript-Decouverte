c91869b77bfc468ea67fd846cac9205920fa2d26
Meikaziku <yanis.marouani13@gmail.com>
2025-10-02 10:41:49 +0200

    exercice confirme 2

---
+++ confirmé/exercice 2/main.js
@@ -0,0 +1,13 @@
+let superHero = [
+  "Spiderman",
+  "Superman",
+  "Deadpool",
+  "TonDaron",
+  "Hulk",
+  "Javascript",
+];
+let phrase = "";
+
+superHero.forEach((phrase) => {
+  console.log(`Je suis fan de ${phrase}`);
+});
